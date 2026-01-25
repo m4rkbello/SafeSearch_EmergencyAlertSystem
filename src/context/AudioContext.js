46d08717be7e27c44808a31cb19db2446af37f14
m4rkbello <markbello10041999@gmail.com>
2026-01-25 13:24:12 +0800

    ADD ALL IMPORTANT FILES

---
+++ src/context/AudioContext.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const AudioContext = () => {
+  return (
+    <View>
+      <Text>AudioContext</Text>
+    </View>
+  )
+}
+
+export default AudioContext
+
+const styles = StyleSheet.create({})
---
+++ src/context/ContactContext.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ContactContext = () => {
+  return (
+    <View>
+      <Text>ContactContext</Text>
+    </View>
+  )
+}
+
+export default ContactContext
+
+const styles = StyleSheet.create({})
