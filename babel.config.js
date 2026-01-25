e46099284eb98a4f23d88a255a1d298a3947b29b
m4rkbello <markbello10041999@gmail.com>
2026-01-25 13:28:35 +0800

    ADD ALL IMPORTANT FILES

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    ['module-resolver', {
+      root: ['./src'],
+      extensions: ['.ios.js', '.android.js', '.js', '.json'],
+      alias: {
+        '@context': './src/context',
+        '@components': './src/components',
+        '@services': './src/services',
+        '@utils': './src/utils',
+        '@screens': './src/screens',
+        '@navigation': './src/navigation',
+      },
+    }],
+    ['module:react-native-dotenv', {
+      moduleName: '@env',
+      path: '.env',
+    }],
+  ],
 };
