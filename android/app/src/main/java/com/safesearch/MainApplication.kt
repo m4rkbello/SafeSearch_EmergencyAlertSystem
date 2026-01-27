65bf69a9aab7eb7c548c38872816bfe249e2665a
m4rkbello <markbello10041999@gmail.com>
2026-01-28 02:59:08 +0800

    added

--- android/app/src/main/java/com/safesearch/MainApplication.kt
+++ android/app/src/main/java/com/safesearch/MainApplication.kt
@@ -5,12 +5,24 @@ import com.facebook.react.ReactApplication
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultReactNativeHost
+import com.facebook.react.shell.MainReactPackage  // ADD THIS IMPORT
 import com.facebook.soloader.SoLoader
 
 class MainApplication : Application(), ReactApplication {
 
     override val reactNativeHost: ReactNativeHost = object : DefaultReactNativeHost(this) {
-        override fun getPackages(): List<ReactPackage> = emptyList()
+        override fun getPackages(): List<ReactPackage> {
+            // You MUST include at least MainReactPackage
+            val packages = mutableListOf<ReactPackage>()
+            packages.add(MainReactPackage())
+            
+            // Add your custom packages here if needed
+            // For example, if you have a custom package:
+            // packages.add(SmsPackage())
+            
+            return packages
+        }
+
         override fun getJSMainModuleName(): String = "index"
         override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
         override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
