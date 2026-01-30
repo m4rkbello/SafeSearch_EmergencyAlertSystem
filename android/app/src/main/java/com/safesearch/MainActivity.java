7ad824ae5f87166f432baa3dfa38027c6b068ba8
m4rkbello <markbello10041999@gmail.com>
2026-01-30 20:58:19 +0800

    abandoned

---
+++ android/app/src/main/java/com/safesearch/MainActivity.java
@@ -0,0 +1,28 @@
+﻿package com.safesearch;
+
+import android.os.Bundle;
+import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactActivityDelegate;
+
+public class MainActivity extends ReactActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
+  }
+
+  @Override
+  protected String getMainComponentName() {
+    return "SafeSearch";
+  }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new DefaultReactActivityDelegate(
+      this,
+      getMainComponentName(),
+      DefaultNewArchitectureEntryPoint.getFabricEnabled()
+    );
+  }
+}
